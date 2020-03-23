acef652a5cd77928ab10aaca2c862da2c94df43d
mmiazi <sakibnm@gmail.com>
2020-03-23 00:40:55 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.9b2a2b5dfe4b58fd737fcb0bd1fe6966.js"
+  "/me/precache-manifest.21fb2e145a96cd5621cb23a7fba8c0d1.js"
 );
 
 workbox.clientsClaim();
