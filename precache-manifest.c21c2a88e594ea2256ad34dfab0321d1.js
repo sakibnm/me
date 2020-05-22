8b7118c916efdcfbadd9bbc68d166ab09e7afbdf
mmiazi <sakibnm@gmail.com>
2020-05-22 00:16:30 -0400

    Updates

--- precache-manifest.c21c2a88e594ea2256ad34dfab0321d1.js
+++ precache-manifest.c21c2a88e594ea2256ad34dfab0321d1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/me/static/js/runtime~main.66be97a1.js"
   },
   {
-    "revision": "ca77746c89a4b8bcc491",
-    "url": "/me/static/js/main.ca77746c.chunk.js"
+    "revision": "b65c3265c95735a03f5f",
+    "url": "/me/static/js/main.b65c3265.chunk.js"
   },
   {
     "revision": "1773584ef7b74a3bb845",
     "url": "/me/static/js/2.1773584e.chunk.js"
   },
   {
-    "revision": "ca77746c89a4b8bcc491",
+    "revision": "b65c3265c95735a03f5f",
     "url": "/me/static/css/main.cffb7c42.chunk.css"
   },
   {
-    "revision": "21ae23a848a1655120eb5cb1760198aa",
+    "revision": "7948c92e9fac7c573dc5a79f35d4a549",
     "url": "/me/index.html"
   }
 ];
